aa7a26ffd04d8e07c01678f7cebf854d166e903f
Brad Carmichael <brad@bradleycarmichael.com>
2016-09-29 12:13:46 -0400

    final changes

--- js/javascript.js
+++ js/javascript.js
@@ -6,6 +6,53 @@
 
 $(function() {
 
+	//smooth scroll begins
+	$('a').smoothScroll();
+
+	$("#startButton").on("click", function(){
+		$.smoothScroll({
+			scrollTarget: "#drinks"
+		});
+	});
+
+	$("input[name='drinks']").on("click", function(){
+			$.smoothScroll({
+				scrollTarget: "#clothing"
+			});
+		});
+
+	$("input[name='clothing']").on("click", function(){
+		$.smoothScroll({
+			scrollTarget: "#music"
+		});
+	});
+
+	$("input[name='music']").on("click", function(){
+		$.smoothScroll({
+			scrollTarget: "#transportation"
+		});
+	});
+
+	$("input[name='transportation']").on("click", function(){
+		$.smoothScroll({
+			scrollTarget: "#style"
+		});
+	});
+
+	$("input[name='style']").on("click", function(){
+		$.smoothScroll({
+			scrollTarget: "#submitButton"
+		});
+	});
+
+	$("#submitButton").on("click", function(){
+		$.smoothScroll({
+			scrollTarget: ".results"
+		});
+	});
+	// smooth scroll ends
+
+	// when submit button is pressed, results appear
 	$("form").on("submit", function(event) {
 		event.preventDefault();
 		var resultMessage = "";
@@ -16,24 +63,22 @@ $(function() {
 
 		var totalScore = 0;
 		for(var i = 0; i < userChoices.length; i = i + 1) {
-			// console.log($(userChoices[i]).val());
 			totalScore = totalScore + Number($(userChoices[i]).val()) ;
 		}
 
 		if(totalScore <= 17) {
-			// console.log("You're not cool!");
 			resultMessage = "you missed the hip train";
 			resultPhoto = "notHipster.jpg";
-			resultParagraph = "Well, well, well.  Like the great Phil Collins song Against All Odds you’re not a hipster.  How did this happen?  Where do you live? Where are you from?  You’re not in the least interested in being cool.  How dare you?  No tight jeans?  How dare you?  No trendy music subculture of stinky beards and thrift shop smelling jacket vests?  How dare you? In reality be proud of yourself for not following along.  You have as strong character with great vision of who you are.  Even though you may wear white socks with Birkenstocks, being a hipster is definitely not you!";
+			resultParagraph = "Well, well, well.  Like the great Phil Collins song, Against All Odds you're not a Hipster.  How did this happen?  Where do you live? Where are you from?  You're not the least bit interested in being Cool?  How dare you?  No tight jeans?  HOW DARE YOU?  No trendy music subculture of stinky beards and thrift shop smelling jacket vests?  How dare you? In reality be proud of yourself for not following along.  You have strong character and a great vision of who you are.  Even though you may wear white socks with Birkenstocks, being a Hipster is definitely not for you!";
 		} else if(totalScore <= 29) {
 			resultMessage = "You're not as hip as you thought";
 			resultPhoto = "midHipster.jpg";
-			resultParagraph = "You don’t even know it but your only one pair of black rimmed glasses away from being a full blown Hipster.  Or maybe a new haircut away that looks just like your better half, whatever the gender. Yes you shop at the Gap or American Eagle so you can’t be a hipster right?!  In theory you’re correct but these stores have redden the wave of the hipster counter culture and the cloths you buy from these establishments are as hipster as ever.  You’re actually riding the wave you just don’t have as surfboard. Stay strong. Stay vigilant. Maybe being a hipster isn’t that bad after all.";
+			resultParagraph = "You don't even know it yet, but you're one pair of black rimmed glasses away from being a full blown Hipster! Yes, you shop at the Gap or American Eagle, so you can't be a Hipster right?!  In theory you're correct but these stores ride the wave of the Hipster counter culture and the clothes you buy from these establishments are as Hipster as ever.  You're actually riding the same wave you just don't have a surfboard. Stay strong. Stay vigilant. Maybe being a Hipster isn't that bad after all.";
 	
 		} else {
 			resultMessage = "you are a definitive hipster!";
 			resultPhoto = "hipsterWinner2.png";
-			resultParagraph = "You value the counter culture.  You are progressive with your politics. You have a deep appreciation for indie music and tighter than tight jeans.  You love American Apparel and are not at odds with their advertising.  You wish you lived in New York but anywhere town wherever will do.  You reject mainstream consumerism by buying all of their second hand shit at thrift stores.  Your androgynous trendy barber cut or Walmart shag allows you to seamlessly fit into most social demographics. At the end of the day however…You’re still just a Hipster!";
+			resultParagraph = "You value the counter culture.  You are progressive with your politics. You have a deep appreciation for indie music and tighter than tight jeans.  You love American Apparel and are not at odds with their advertising.  You wish you lived in New York but anywhere town, wherever will do.  You reject mainstream consumerism by buying all of your shit at second hand thrift stores.  Your androgynous trendy barber cut or Walmart shag allows you to seamlessly fit into most social demographics. At the end of the day however…You're still just, a Hipster!";
 		}
 
 		var finalAnswer = $('<h3>').text(resultMessage);
@@ -44,7 +89,17 @@ $(function() {
 
 		var finalPhoto = $('<img src="images/' + resultPhoto + '">');
 		$("div.results").append(finalPhoto);
+
+		//shows retake quiz button
+		$("button.reset").removeClass("hidden");
 	
+	}); // closes form submit
+
+	// Refreshes page when button is clicked
+	$(".reset").on("click", function(){
+		$("body").scrollTop(0);
+		document.location.reload(true);
 	});
 
-});
+
+}); // closes document ready
